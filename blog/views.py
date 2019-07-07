d827059daa76c308a9f1b762f72d3eaf0f639dac
Shahrooz M <sibche35@gmail.com>
2019-07-07 15:36:41 +0430

    created models and registered in admin gui, use models instead of domy data in templates

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-posts = [
-        {
-            'title':'this is a title for test',
-            'author':'shahrooz mohammadloo', 
-            'date_posted':'April, 2, 2019', 
-            'content':'Amet ducimus maiores delectus accusamus velit illo Fugiat ratione et placeat impedit blanditiis Ipsa voluptatem ullam maiores perspiciatis aliquam. A mollitia fuga veritatis unde sint Sed deleniti tenetur consequatur facilis?',
-
-
-            },
-        {
-            'title':'Second title for tesst',
-            'author':'Alex toxin', 
-            'date_posted':'Augost, 8, 2019', 
-            'content':'Amet ducimus maiores delectus accusamus velit illo Fugiat ratione et placeat impedit blanditiis Ipsa voluptatem ullam maiores perspiciatis aliquam. A mollitia fuga veritatis unde sint Sed deleniti tenetur consequatur facilis?',
-
-
-            }
-        ]
 
 def index(request):
-    domy = {'posts':posts}
+    domy = {'posts':Post.objects.all()}
     return render(request, 'blog/index.html', domy)
 
 
